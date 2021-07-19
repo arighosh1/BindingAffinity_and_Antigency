80d133ae4a707aa3b9d3818e1555a5267f78add9
arighosh1 <aritra.tech@gmail.com>
2021-07-19 20:32:20 +0500

    Update binding_ML1.py

--- binding_ML1.py
+++ binding_ML1.py
@@ -5,6 +5,30 @@ import numpy as np
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 import streamlit as st
+from Bio.SeqUtils.ProtParam import ProteinAnalysis
+from PIL import Image
+from quantiprot.metrics.aaindex import get_aa2charge, get_aa2hydropathy
+img=Image.open('icon.jpeg')
+st.set_page_config(page_title='RATIONAL VACCINE DESIGN FOR VIRUS USING MACHINE LEARNING APPROACHES', page_icon =img, layout = 'wide', initial_sidebar_state = 'auto')
+import os
+
+basedir = os.path.dirname(os.path.abspath(__file__))
+basedir+="/icon.jpg"
+st.markdown(
+
+    """
+    <style>
+    .reportview-container {
+        background: url("""+basedir+""")
+    }
+   .sidebar .sidebar-content {
+        background: url("url_goes_here")
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 
 st.sidebar.title("Chose From SlideBar")
 value = st.sidebar.slider("slide to 0 for  Affinity and 1 for Antigency",0,1)
@@ -39,7 +63,48 @@ if value==0:
         # sns.distplot(random.poisson(lam=test_features), hist=False, label='poisson')
         # st.write(plt.show())
 elif value==1:
-    st.title("Working on code.")
+    # !/usr/bin/env python
+    # coding: utf-8
+
+    # In[51]:
+
+    file = ''
+    uploaded_files = st.file_uploader("Choose a Protein File To Check Its Antigenicity : ", accept_multiple_files=True)
+    for uploaded_file in uploaded_files:
+        file = uploaded_file.read()
+
+    if file != '':
+        X = ProteinAnalysis(str(file))
+
+
+        alanine = X.get_amino_acids_percent()['A']
+        valine = X.get_amino_acids_percent()['V']
+        isoleucine = X.get_amino_acids_percent()['I']
+        leucine = X.get_amino_acids_percent()['L']
+        sec_struc = X.secondary_structure_fraction()
+            # st.write("Secondary structure fraction = ",sec_struc[0])
+        epsilon_prot = X.molar_extinction_coefficient()
+            # st.write(epsilon_prot)
+
+            # In[52]:
+
+            # Aliphatic index = X(Ala) + a * X(Val) + b * ( X(Ile) + X(Leu) )
+        Aliphatic_index = ((alanine + 2.9) * (valine + 3.9) * (isoleucine + leucine)) / 10
+
+            # In[53]:
+
+        st.write("Aliphatic Index Value Is : ",Aliphatic_index)
+
+        # In[54]:
+
+        if Aliphatic_index >= 0.1:
+            st.write("The Protein is antigenic protein")
+        else:
+            st.write("The Protein is non-antigenic protein")
+
+else:
+    st.write("Input Your Sequence")
+
 
 
 

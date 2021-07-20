30f19f1116819af71ecadab4b261466219151d28
arighosh1 <aritra.tech@gmail.com>
2021-07-21 01:34:21 +0500

    Update binding_ML1.py

--- binding_ML1.py
+++ binding_ML1.py
@@ -21,7 +21,7 @@ st.markdown(
     """
     <style>
     .reportview-container {
-        background: url(https://github.com/arighosh1/BindingAffinity_and_Antigency/blob/main/icon.jpeg?raw=true);
+        background: url();
         height: 100%;
         background-position: center;
         background-repeat: no-repeat;

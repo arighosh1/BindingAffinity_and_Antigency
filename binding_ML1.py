c17c278d4e3c6ae72a4c255870105e5cafd89d63
arighosh1 <aritra.tech@gmail.com>
2021-08-12 10:11:15 -0400

    Update binding_ML1.py

--- binding_ML1.py
+++ binding_ML1.py
@@ -459,7 +459,7 @@ elif value == 1:
     protein = st.text_input("Enter Protein Sequence ")
     # calculate averaged Hopp score
     if len(protein) > 0:
-        pep_length=st.slider("Window Size : ",3,7,9,11,13,15,17,19,21,3)
+        pep_length=st.slider("Window Size : ",3,7,9,3)
         alpha=st.slider("Alpha value : ",0.1,1,0.1)
         result = calc_hopp(protein, pep_length,alpha)
 

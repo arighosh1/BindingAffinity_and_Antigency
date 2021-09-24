ab8da3b7533d499ac96bfdc39662a913df20e55b
arighosh1 <aritra.tech@gmail.com>
2021-09-24 23:33:52 +0500

    Update binding_ML1.py

--- binding_ML1.py
+++ binding_ML1.py
@@ -47,7 +47,7 @@ st.markdown(
     ''',
     unsafe_allow_html=True
 )
-st.sidebar.image("https://github.com/arighosh1/BindingAffinity_and_Antigency/blob/main/foo.jpg?raw=true")
+st.sidebar.image("https://github.com/arighosh1/BindingAffinity_and_Antigency/blob/main/icon1.jpg?raw=true")
 value=st.sidebar.slider("Slide to 0 for Binding Affinity And 1 for Antigenicity and 2 for Covid_Autoimmune",0,2,0)
 if value == 0:
     # !/usr/bin/env python
@@ -286,7 +286,7 @@ if value == 0:
             plt.savefig('foo.jpg')
             from PIL import Image
 
-            image = Image.open('foo.jpg')
+            image = Image.open("https://github.com/arighosh1/BindingAffinity_and_Antigency/blob/main/foo.jpg?raw=true")
             st.image(image)
 
             # In[ ]:

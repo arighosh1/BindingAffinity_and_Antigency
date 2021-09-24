7b0cc479744902c6bd2bba6a178b62f22b8f6746
arighosh1 <aritra.tech@gmail.com>
2021-09-24 23:41:00 +0500

    Update binding_ML1.py

--- binding_ML1.py
+++ binding_ML1.py
@@ -283,11 +283,10 @@ if value == 0:
             test_y = lab_enc.fit_transform(y_test)
             print(metrics.accuracy_score(test_y, y_pred))
             plt.scatter(x_test[:, 0], x_test[:, 1], c=test_y)
-            plt.savefig('foo.jpg')
+            plt.savefig("foo", dpi=100)
             from PIL import Image
 
-            image = Image.open("https://github.com/arighosh1/BindingAffinity_and_Antigency/blob/main/foo.jpg?raw=true")
-            st.image(image)
+            st.image("foo.png")
 
             # In[ ]:
 

cd1b02906ac7c685d0061146a102e2b36cebfa5e
arighosh1 <aritra.tech@gmail.com>
2021-10-08 09:15:59 -0400

    Update binding_ML1.py

--- binding_ML1.py
+++ binding_ML1.py
@@ -878,15 +878,7 @@ elif value == 2:
                       -0.618, -0.247, -0.097, 0.344, 0.221, 0.256, 0.115
                       ]
 
-plt.figure(figsize=(12,6))
-plt.plot(x, y, "r-", linewidth=7, alpha=0.4)
-plt.plot(x, y_expassy, "b--")
-plt.title("Comparison of Script Result vs Expasy Result (alpha=0.1, Window=7)")
-plt.xlabel("Amino Acid Position")
-plt.ylabel("Hydrophilicity Score of Peptide")
-plt.legend(["Script", "Expasy"], loc="lower right")
 
-#plt.savefig("expassy_validate_weighted.png", dpi=300)
 
 elif value == 3:
 
